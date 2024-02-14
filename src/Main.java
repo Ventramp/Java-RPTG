a31c405c3064cdaa90f55c768733df360392df68
ferrdz <fernanda.Rodriguezg15@my.unitec.edu.mx>
2024-02-13 19:04:29 -0600

    basic player + player

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,11 @@ import characters.Jobs.Ranger;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\n//////////BIENVENIDO//////////");
-     Ranger ranger = new Ranger();
-     ranger.newName();
-     ranger.displayData();
-     ranger.attack();
-
+        BasicCharacter basic = new BasicCharacter();
+        basic.newName();
+        basic.displayData();
+        Player player = new Player();
+        player.displayData();
+        player.attack();
     }
 }
