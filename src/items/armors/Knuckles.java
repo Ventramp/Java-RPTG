272cfee7d181da226ba3176f75227b108bdba620
ferrdz <fernanda.Rodriguezg15@my.unitec.edu.mx>
2024-02-16 16:04:14 -0600

    "Knuckles y Sword"

--- src/items/armors/Knuckles.java
+++ src/items/armors/Knuckles.java
@@ -2,4 +2,12 @@ package items.armors;
 
 
 public class Knuckles extends Armors {
+    public Knuckles() {
+        this.name = "Brazaletes Oxidados";
+        this.description = "Bazaletes de hierro Oxidado, Los encontraste tirados por ahi";
+        this.price = 0;
+        this.lv = 1;
+        this.aHp = 1;
+        this.aDef = 1;
+    }
 }
--- src/items/weapons/Sword.java
+++ src/items/weapons/Sword.java
@@ -1,4 +1,11 @@
 package items.weapons;
 
 public class Sword extends Weapons {
+    public Sword() {
+        this.name = "Espada Oxidada";
+        this.description = "Una espada que encontraste tirada en el suelo, parece que se rompera en cualquier segundo";
+        this.price = 0;
+        this.wAttk = 2;
+        this.lv = 1;
+    }
 }
