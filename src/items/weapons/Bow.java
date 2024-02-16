46fd044753c43fb33fc8ff7b17e0abda5a12d34f
Elena <laraelena2345678@gmail.com>
2024-02-15 21:12:26 -0600

    "caracteristicas de arco"

--- src/items/weapons/Bow.java
+++ src/items/weapons/Bow.java
@@ -1,4 +1,11 @@
 package items.weapons;
 
-public class Bow extends Weapons {
+public class bow extends Weapons {
+    public bow() {
+        this.name = "Arco de madera";
+        this.description = "Un arco de madera vieja y mohosa, lo encontraste tirado por ahi";
+        this.price = 0;
+        this.wAttk = 1;
+        this.lv = 1;
+    }
 }
